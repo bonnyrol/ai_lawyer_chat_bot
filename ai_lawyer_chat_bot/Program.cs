bd030e59583f559598c253d09b52544126d59a8d
bonnyrol <Artemkar1032@gmail.com>
2025-11-28 16:41:38 +0500

    proj init

--- ai_lawyer_chat_bot/Program.cs
+++ ai_lawyer_chat_bot/Program.cs
@@ -1,2 +1,4 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using dotenv.net;
+DotEnv.Load();
+var envVars = DotEnv.Read();
+Console.WriteLine(envVars["TG_TOKEN"]);
