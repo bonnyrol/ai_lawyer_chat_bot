d27e1511599e62a13a7fb909008f118ae132ac4c
noob <noob@WIN-C0H1J8OND37>
2025-11-28 16:23:25 +0500

    Добавьте файлы проекта.

---
+++ lawyer_tests/UnitTest1.cs
@@ -0,0 +1,16 @@
+namespace lawyer_tests
+{
+    public class Tests
+    {
+        [SetUp]
+        public void Setup()
+        {
+        }
+
+        [Test]
+        public void Test1()
+        {
+            Assert.Pass();
+        }
+    }
+}
